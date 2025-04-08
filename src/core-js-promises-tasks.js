7dce7e3abe3c57b50ba19f5dfa97a87ccc6158d7
godlikeAlex <godlikedesigner@gmail.com>
2025-04-08 23:50:23 +0500

    feat: resolve getAllOrNothing task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -106,8 +106,14 @@ async function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+async function getAllOrNothing(promises) {
+  try {
+    const result = await Promise.all(promises);
+
+    return result;
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
 
 /**
