20950d52f96a97d3d83ca0d0f1df3e7792548987
godlikeAlex <godlikedesigner@gmail.com>
2025-04-08 23:46:16 +0500

    feat: resolve getFirstPromiseResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -85,8 +85,14 @@ async function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+async function getFirstPromiseResult(promises) {
+  try {
+    const firstResult = await Promise.race(promises);
+
+    return firstResult;
+  } catch {
+    return Promise.reject();
+  }
 }
 
 /**
